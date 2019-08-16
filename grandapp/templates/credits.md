4fdfbf7e2292945e246971ebc88592e644f28118
marouenbg <marouen.b.guebila@gmail.com>
2019-08-16 11:49:00 -0400

    switched to django

---
+++ grandapp/templates/credits.md
@@ -0,0 +1,6 @@
+free vector
+
+drugs
+<a href="https://www.freepik.com/free-photos-vectors/abstract">Abstract vector created by freepik - www.freepik.com</a>
+cell lines
+<a href="https://www.freepik.com/free-photos-vectors/infographic">Infographic vector created by brgfx - www.freepik.com</a>
---
+++ granddb/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from django.contrib import admin
+
+from grandapp import views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home, name='home'),
+    url(r'^about/' ,views.about, name='about'),
+    #url(r'^adoptions/(\d+)/', views.pet_detail, name='pet_detail'),
+]
