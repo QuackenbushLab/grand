306bea004cf032e2ad2b3f0966b7506566c9ecc1
marouenbg <marouen.b.guebila@gmail.com>
2019-09-06 16:49:36 -0400

    add new drugs csv

--- README.md
+++ README.md
@@ -15,3 +15,17 @@ make migration
 run migration
 
 `python3 manage.py migrate`
+
+Requires
+
+`pip: django django-widget-tweaks gunicorn psycopg2-binary djangorestframework-datatables djangorestframework`
+
+`apt: python3-pip python3-dev libpq-dev postgresql-contrib nginx curl`
+
+Firewall
+
+`sudo ufw allow 8000`
+
+Launch server
+
+`python3 manage.py runserver 0.0.0.0:8000`
--- src/buildDrugDf.py
+++ src/buildDrugDf.py
@@ -3,6 +3,10 @@ import pandas as pd
 import numpy as np
 import os
 
+#Read network dimension
+os.chdir('/Users/mab8354/granddb/src')
+dimNet = pd.read_csv('dimNet.csv')
+
 # Initialize dataframe
 df = pd.DataFrame(columns=['number','drug','tool','netzoo','network','ppi','motif','expression','tfs','genes','refs'])
 
@@ -21,6 +25,7 @@ numbersVec    = []
 drugVec       = []
 ppiVec        = []
 refsVec       = []
+missing=0
 for line in res.splitlines():
     print(line)
     nDrugs = nDrugs + 1
@@ -28,11 +33,19 @@ for line in res.splitlines():
     drugList.append(drug)
     networkVec.append('<a href="' + 'https://granddb.s3.amazonaws.com/drugs/drugNetworks/' + drug + '.txt.mat' + '" download><i class="fas fa-download"></i></a>')
     expressionVec.append('<a href="' + 'https://granddb.s3.amazonaws.com/drugs/drugExpression/' + drug + '.txt' + '"><i class="fas fa-download"></i></a> <a href="#"><i class="fas fa-link"></i></a>')
-    tfsVec.append(nDrugs)
-    genesVec.append(nDrugs)
     numbersVec.append(nDrugs)
     drugVec.append('<a href = "' + '#' + '" >' + drug + '</a>')
     refsVec.append('<a href="#"><i class="fas fa-book"></i></a>')
+    matching = [s for s in range(len(dimNet.iloc[:,0])) if drug+'.txt.mat' == dimNet.iloc[s,0]]
+    if matching != []:
+        tfsVec.append(dimNet.iloc[matching,1].values[0])
+        genesVec.append(dimNet.iloc[matching,2].values[0])
+    else:
+        tfsVec.append(0)
+        genesVec.append(0)
+        missing=missing+1 # 3 are missing
+
+
 
 # build vectors
 toolVec       = np.repeat('PANDA', nDrugs)
