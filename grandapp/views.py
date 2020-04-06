42fe62f4b405464a32b25e238fd0593212d4252a
marouenbg <marouen.b.guebila@gmail.com>
2020-04-06 19:19:37 -0400

    fix slug3

--- grandapp/views.py
+++ grandapp/views.py
@@ -292,6 +292,8 @@ def tissuelandingerror(request,slug):
                         resURL='https://granddb.s3.amazonaws.com/tissues/networks/lioness/singleSample/' + fileName
             except:
                 if download_sample == 'all':
+                    slug3=[t.capitalize() for t in slug.split('_')]
+                    slug3='_'.join(slug3)
                     resURL = 'https://granddb.s3.amazonaws.com/tissues/networks/lioness/' + slug3[:-7] + '_AllSamples.csv'
                 else:
                     return redirect('tissuelandingerror',slug)
